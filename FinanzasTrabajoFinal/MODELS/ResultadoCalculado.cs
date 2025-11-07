be6a12e88164d4cfa52f3810db4759dafd553029
Erick Mayorquin <mayorquinerick2006@gmail.com>
2025-11-07 00:07:37 -0600

    Nuevas carpetas y razors

---
+++ FinanzasTrabajoFinal/MODELS/ResultadoCalculado.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+// Asegúrate de que el namespace sea el mismo de tus otros modelos
+namespace FinanzasTrabajoFinal.MODELS
+{
+    // Esta es la clase que sacamos de Resultados.razor
+    public class ResultadoCalculado
+    {
+        public string Concepto { get; set; } = "";
+        public Dictionary<int, decimal> ValoresPorAnio { get; set; } = new();
+    }
+}
