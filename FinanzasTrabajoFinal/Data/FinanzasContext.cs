08fa3d5bcf3bfb30fb745db79b14b6ff19bff87c
Rodri-Zelaya <rzelayasalinas7@gmail.com>
2025-11-16 22:54:53 -0600

    CORREGIDO DUPONT

--- FinanzasTrabajoFinal/Data/FinanzasContext.cs
+++ FinanzasTrabajoFinal/Data/FinanzasContext.cs
@@ -21,7 +21,7 @@ public class FinanzasContext : DbContext
     public DbSet<OrigenAplicacion> OrigenAplicaciones { get; set; }
     public DbSet<CapitalesNetos> CapitalesNetos { get; set; }
     public DbSet<FlujoEfectivo> FlujosEfectivo { get; set; }
-
+    public DbSet<DuPont> DuPonts { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
---
+++ FinanzasTrabajoFinal/MODELS/DuPont.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanzasTrabajoFinal.MODELS
+{
+    [Table("DUPONT")]
+    public class DuPont
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID_DUPONT")]
+        public int IdDuPont { get; set; }
+
+        [Column("ID_ANALISIS")]
+        public int IdAnalisis { get; set; }
+
+        [Column("ANIO")]
+        public int Anio { get; set; }
+
+        [Column("MARGEN_UTILIDAD", TypeName = "decimal(18, 6)")]
+        public decimal MargenUtilidad { get; set; }
+
+        [Column("ROTACION_ACTIVO", TypeName = "decimal(18, 6)")]
+        public decimal RotacionActivo { get; set; }
+
+        [Column("APALANCAMIENTO", TypeName = "decimal(18, 6)")]
+        public decimal Apalancamiento { get; set; }
+
+        [Column("ROA", TypeName = "decimal(18, 6)")]
+        public decimal ROA { get; set; }
+
+        [Column("ROE", TypeName = "decimal(18, 6)")]
+        public decimal ROE { get; set; }
+
+        [ForeignKey(nameof(IdAnalisis))]
+        public AnalisisFinanciero AnalisisFinanciero { get; set; }
+    }
+}
+
