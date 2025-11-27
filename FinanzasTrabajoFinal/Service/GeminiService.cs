08bc99ff893fbaa0d078c537ffed124203e113b3
Erick Mayorquin <mayorquinerick2006@gmail.com>
2025-11-26 21:59:08 -0600

    Que funcione

--- FinanzasTrabajoFinal/Service/GeminiService.cs
+++ FinanzasTrabajoFinal/Service/GeminiService.cs
@@ -12,7 +12,7 @@ namespace FinanzasTrabajoFinal.Service
         private readonly HttpClient _httpClient;
 
         // Tu clave (La vi en tus capturas, está bien)
-        private const string ApiKey = "AIzaSyChmBW8y8igJglGOT5n_oasX8Jz4ylj0qQ";
+        private const string ApiKey = "AIzaSyAQxP74Ups3Ap80QPgjQR6ehxjSRnd27Mo";
 
         // === EL CAMBIO FINAL ===
         // 1. Usamos 'v1beta' (Obligatorio para modelos 1.5)
